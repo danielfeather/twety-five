146d154c06a93ff3fcedd1e2ea2b85a49d019387
Daniel Feather <danielfeather@users.noreply.github.com>
2023-08-06 12:48:41 +0100

    Nx Migration

---
+++ apps/twety-five/postcss.config.js
@@ -0,0 +1,12 @@
+const { join } = require('path')
+
+const config = {
+    plugins: {
+        tailwindcss: {
+            config: join(__dirname, 'tailwind.config.ts')
+        },
+        autoprefixer: {}
+    }
+}
+
+module.exports = config
--- apps/twety-five/tailwind.config.ts
+++ apps/twety-five/tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from 'tailwindcss'
+
+export default {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
@@ -7,5 +8,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
-
+} satisfies Config
